121026b6b95e9574eb9d961adc168c3281bdeab9
savadom13 <savadom@ya.ru>
2016-11-21 23:25:12 +0200

    task 2.3: models upd, modelmanager created

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from django.template import Context, loader
 
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
+
+
+def index(request):
+    aaa = 'aaa'
+
+    template = loader.get_template('index.html')
+    context= Context({
+        "aaa": aaa,
+    })
+    return HttpResponse(template.render(context))
